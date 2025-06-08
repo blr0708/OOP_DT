ca0583b072af6c36d1028ec4468e18676ee37c04
blr0708 <dimkingugl@gmail.com>
2025-06-08 16:38:04 +0200

    Add files via upload

---
+++ Testy/src/main/java/music/Playlist.java
@@ -0,0 +1,15 @@
+package music;
+
+import java.util.ArrayList;
+
+public class Playlist extends ArrayList<Song>
+{
+    void atSecond
+    {
+        Playlist playlist = new Playlist();
+        userNull(playlist.atSecond(45));
+        Song songToAdd = new Song("As", "ASD", 55);
+        playlist.add(SongToAdd);
+        
+    }
+}
---
+++ Testy/src/test/java/music/PlaylistTest.java
@@ -0,0 +1,8 @@
+package music;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaylistTest {
+    @Test
+    public void isAddedSongIsTheSameObjectOnLIst
+}
