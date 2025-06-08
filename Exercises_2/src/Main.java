1f6d4d725562fe1bb8304d25638a6a1c4618c457
blr0708 <dimkingugl@gmail.com>
2025-06-08 19:42:58 +0200

    Update Main.java

--- Exercises_2/src/Main.java
+++ Exercises_2/src/Main.java
@@ -3,33 +3,33 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        SvgScene scene = new SvgScene(); // Создаем сцену
+        SvgScene scene = new SvgScene(); // Tworzenie sceny
 
-        // Создаем 5 многоугольников
+        // Tworzymy 5 wielokątów
         Point[] p1 = {new Point(10, 10), new Point(60, 10), new Point(35, 50)};
         Point[] p2 = {new Point(100, 100), new Point(150, 100), new Point(125, 150)};
         Point[] p3 = {new Point(200, 200), new Point(250, 200), new Point(225, 250)};
         Point[] p4 = {new Point(300, 300), new Point(350, 300), new Point(325, 350)};
         Point[] p5 = {new Point(400, 400), new Point(450, 400), new Point(425, 450)};
 
-        // Добавляем их в сцену
+        // Dodajemy je do sceny
         scene.addPolygon(new Polygon(p1));
         scene.addPolygon(new Polygon(p2));
         scene.addPolygon(new Polygon(p3));
-        scene.printPolygons(); // Должно вывести 3 многоугольника
+        scene.printPolygons(); // Powinny zostać wyświetlone 3 wielokąty
 //
-//        System.out.println("Добавляем еще 2...");
-//        scene.addPolygon(new Polygon(p4)); // Перезапишет p1
-//        scene.addPolygon(new Polygon(p5)); // Перезапишет p2
-//        scene.printPolygons(); // Должны остаться p3, p4, p5
+//        System.out.println(" Dodajemy jesczce 2...");
+//        scene.addPolygon(new Polygon(p4)); //  p1
+//        scene.addPolygon(new Polygon(p5)); //  p2
+//        scene.printPolygons(); // Muszą zostać p3, p4, p5
 
         String svgContent = scene.toSvg();
-        System.out.println(svgContent); // Выводим SVG-контент на экран
+        System.out.println(svgContent); // Wyświetlanie treści SVG na ekranie
 
-        // Записываем SVG в файл
+        // Zapis SVG do pliku
         try (FileWriter writer = new FileWriter("scene_with_triangles.svg")) {
             writer.write(svgContent);
-            System.out.println("SVG файл успешно сохранен как scene_with_triangles.svg!");
+            System.out.println("Plik SVG został pomyślnie zapisany jako scene_with_triangles.svg!");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,21 +38,21 @@ public class Main {
 //7 8
 //public class Main {
 //    public static void main(String[] args) {
-//        SvgScene scene = new SvgScene(); // Создаем сцену
+//        SvgScene scene = new SvgScene(); // Tworzenie sceny
 //
-//        // Создаем 5 многоугольников
+//        // Tworzymy 5 wielokątów
 //        Point[] p1 = {new Point(10, 10), new Point(60, 10), new Point(35, 50)};
 //        Point[] p2 = {new Point(100, 100), new Point(150, 100), new Point(125, 150)};
 //        Point[] p3 = {new Point(200, 200), new Point(250, 200), new Point(225, 250)};
 //        Point[] p4 = {new Point(300, 300), new Point(350, 300), new Point(325, 350)};
 //        Point[] p5 = {new Point(400, 400), new Point(450, 400), new Point(425, 450)};
 //
-//        // Добавляем их в сцену
+//        // Dodajemy je do sceny
 //        scene.addPolygon(new Polygon(p1));
 //        scene.addPolygon(new Polygon(p2));
 //        scene.addPolygon(new Polygon(p3));
 //
-//        // Сохраняем в файл
+//        // Zapisywanie do pliku
 //        scene.save("scene_with_bounding_box.svg");
 //    }
 //}
