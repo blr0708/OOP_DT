209422387cd5c647d85f5293e6887ac50bb7e46a
blr0708 <dimkingugl@gmail.com>
2025-06-29 18:45:00 +0200

    Update TestDot.java

--- Powtorzenie_do_drugiego_kolokwium/src/main/java/org/example/powtorzenie_do_drugiego_kolokwium/shared/TestDot.java
+++ Powtorzenie_do_drugiego_kolokwium/src/main/java/org/example/powtorzenie_do_drugiego_kolokwium/shared/TestDot.java
@@ -4,14 +4,14 @@ import javafx.scene.paint.Color;
 
 public class TestDot {
     public static void main(String[] args) {
-        // 🔴 Создаём объект Dot
+        
         Dot dot = new Dot(150, 200, Color.RED, 25);
 
-        // 🟠 Преобразуем в строку
+        
         String msg = Dot.toMessage(dot.x(), dot.y(), dot.color(), dot.radius());
         System.out.println("Zakodowana wiadomość: " + msg);
 
-        // 🟢 Снова превращаем строку в объект
+        
         Dot parsed = Dot.fromMessage(msg);
         System.out.println("Odczytany X: " + parsed.x());
         System.out.println("Odczytany Y: " + parsed.y());
